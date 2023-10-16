4b7fab973e5d9983632b8557df5ca2877ddb1b91
Alysson Oliveira <3oalysson@gmail.com>
2023-10-16 18:15:26 -0300

    Inicial demo

---
+++ src/main/java/com/senac/swaggerdemo/SwaggerDemoApplication.java
@@ -0,0 +1,24 @@
+package com.senac.swaggerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@RestController
+@EnableSwagger2
+public class SwaggerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwaggerDemoApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "Swagger") String name) {
+        return String.format("Hello %s! This is a simple Swagger demo.", name);
+    }
+}
+
